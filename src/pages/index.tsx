b7d7da404bbf21d7563a85e8f9c4e9cf42b3a229
sia <luosianao@gmail.com>
2024-11-25 12:01:01 +0800

    add header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,20 +4,29 @@ import React from "react";
 
 import CssBaseline from '@mui/material/CssBaseline';
 import AppTheme from '../theme/AppTheme';
-import Features  from "../theme/Features";
+import Features from "../theme/Features";
 import Hero from '../theme/Hero';
 import Footer from "../theme/Footer";
-
-
+import Head from "next/head";
 export default function Home() {
   return (
-    <AppTheme >
-      <CssBaseline enableColorScheme />
-      <Hero />
-    <Features></Features>
-      <AppAppBar></AppAppBar>
-      <Footer></Footer>
-    </AppTheme>
+    <>
+      <Head>
+      <title>GitHub Proxy 代理加速
+      </title>
+        <meta name='description' content='Github Proxy Github 国内代理 GitHub 文件 , Releases , archive , gist , raw.githubusercontent.com 文件代理加速下载服务.' />
+        <meta name='keywords' content={`Github Proxy Github 国内代理 GitHub 文件 , Releases `} />
+      
+      </Head>
+      <AppTheme >
+        <CssBaseline enableColorScheme />
+        <Hero />
+        <Features></Features>
+        <AppAppBar></AppAppBar>
+        <Footer></Footer>
+      </AppTheme>
+    </>
+
   );
 }
 
