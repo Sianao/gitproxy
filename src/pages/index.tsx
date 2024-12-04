07c747499374e43780f18d593aa6a5e129e1ff77
sia <luosianao@gmail.com>
2024-12-04 20:24:08 +0800

    fix err meta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,10 +16,10 @@ export default function Home() {
       </title>
         <link rel="icon" href="favicon.ico" type="image/x-icon"/>
         <meta name='description' content='Github Proxy Github 国内代理 GitHub 文件 , Releases , archive , gist , raw.githubusercontent.com 文件代理加速下载服务.' />
-        <meta property="og:url" content="https://gitproxy.click"></meta>
-        <meta http-equiv='content-language' content='zh-CN'> </meta>
-        <meta property="og:title" content="GitHub Proxy 代理加速"></meta>
-        <meta property="og:description" content="GitHub 文件 , Releases , archive , gist , raw.githubusercontent.com 文件代理加速下载服务."></meta>
+        <meta property="og:url" content="https://gitproxy.click"/>
+        <meta http-equiv='content-language' content='zh-CN'/> 
+        <meta property="og:title" content="GitHub Proxy 代理加速"/>
+        <meta property="og:description" content="GitHub 文件 , Releases , archive , gist , raw.githubusercontent.com 文件代理加速下载服务."/>
         <meta name='keywords' content={`Github Proxy Github 国内代理 GitHub 文件 , Releases `} />
       
       </Head>
